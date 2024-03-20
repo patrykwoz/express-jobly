10fab7c981745009c28b9b11a84e9ed32bbc94c2
Patryk Wozniczka <patryk.wozniczka@gmail.com>
2024-03-19 22:50:10 -0400

    Finished Setup

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const authRoutes = require("./routes/auth");
 const companiesRoutes = require("./routes/companies");
 const usersRoutes = require("./routes/users");
 
-const morgan = require("morgan");
+// const morgan = require("morgan");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use(authenticateJWT);
 
 app.use("/auth", authRoutes);
@@ -33,7 +33,10 @@ app.use(function (req, res, next) {
 
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  if (process.env.NODE_ENV !== "test") {
+    console.error(err.stack);
+  }
+
   const status = err.status || 500;
   const message = err.message;
 
